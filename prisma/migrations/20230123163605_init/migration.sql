9dec2f6f317a103fbdbba725bd635f794c0b59af
Yuta123456 <yuuta09090530@icloud.com>
2023-01-24 01:37:23 +0900

    DB生成

---
+++ prisma/migrations/20230123163605_init/migration.sql
@@ -0,0 +1,51 @@
+/*
+  Warnings:
+
+  - You are about to drop the `User` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+DROP TABLE "User";
+
+-- CreateTable
+CREATE TABLE "Student" (
+    "id" TEXT NOT NULL,
+
+    CONSTRAINT "Student_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Reservation" (
+    "id" SERIAL NOT NULL,
+    "seat" INTEGER NOT NULL,
+    "period" INTEGER NOT NULL,
+    "date" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Reservation_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "ReservationStudent" (
+    "reservationStudentId" SERIAL NOT NULL,
+    "studentId" TEXT NOT NULL,
+    "reservationId" INTEGER NOT NULL,
+
+    CONSTRAINT "ReservationStudent_pkey" PRIMARY KEY ("reservationStudentId")
+);
+
+-- CreateTable
+CREATE TABLE "StudentLedger" (
+    "id" INTEGER NOT NULL,
+    "studentId" TEXT NOT NULL,
+
+    CONSTRAINT "StudentLedger_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "ReservationStudent" ADD CONSTRAINT "ReservationStudent_studentId_fkey" FOREIGN KEY ("studentId") REFERENCES "Student"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ReservationStudent" ADD CONSTRAINT "ReservationStudent_reservationId_fkey" FOREIGN KEY ("reservationId") REFERENCES "Reservation"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "StudentLedger" ADD CONSTRAINT "StudentLedger_studentId_fkey" FOREIGN KEY ("studentId") REFERENCES "Student"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
