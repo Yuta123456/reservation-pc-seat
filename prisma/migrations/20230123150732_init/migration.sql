e63e81adf3d115fc15a4197ac8ad2f6fdb9bc276
Yuta123456 <yuuta09090530@icloud.com>
2023-01-24 00:15:27 +0900

    local docker db 作成

---
+++ prisma/migrations/20230123150732_init/migration.sql
@@ -0,0 +1,22 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `email` on the `User` table. All the data in the column will be lost.
+  - You are about to drop the column `name` on the `User` table. All the data in the column will be lost.
+  - You are about to drop the `Post` table. If the table is not empty, all the data it contains will be lost.
+  - Added the required column `studentId` to the `User` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- DropForeignKey
+ALTER TABLE "Post" DROP CONSTRAINT "Post_authorId_fkey";
+
+-- DropIndex
+DROP INDEX "User_email_key";
+
+-- AlterTable
+ALTER TABLE "User" DROP COLUMN "email",
+DROP COLUMN "name",
+ADD COLUMN     "studentId" TEXT NOT NULL;
+
+-- DropTable
+DROP TABLE "Post";
