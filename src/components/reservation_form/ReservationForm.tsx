21f88ba2c77120ffb9fa613f1fc739fceb75495f
Yuta123456 <yuuta09090530@icloud.com>
2023-01-24 03:47:04 +0900

    method一つ作って、DB確認した

--- src/components/reservation_form/ReservationForm.tsx
+++ src/components/reservation_form/ReservationForm.tsx
@@ -111,12 +111,18 @@ export const ReservationForm: FC<ReservationFormProps> = ({
             colorScheme="blue"
             isDisabled={studentsIds.some((v) => v.length === 0)}
             onClick={() => {
-              onClose();
-              toast({
-                title: "予約しました",
-                status: "success",
-                duration: 2000,
-                isClosable: true,
+              fetch("api/reservation", {
+                method: "POST",
+                body: JSON.stringify({ seat, period, studentsIds }),
+              }).then(async (res) => {
+                console.log(await res.json());
+                onClose();
+                toast({
+                  title: "予約しました",
+                  status: "success",
+                  duration: 2000,
+                  isClosable: true,
+                });
               });
             }}
           >
