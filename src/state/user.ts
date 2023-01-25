f4f9de1a28108d3417d291ccc4fb3d56d559d1d4
Yuta123456 <yuuta09090530@icloud.com>
2023-01-26 01:20:08 +0900

    stateの永続化とコメント削除

--- src/state/user.ts
+++ src/state/user.ts
@@ -13,19 +13,5 @@ export const userState = atom<User>({
     id: "",
     role: "",
   },
-  // effects_UNSTABLE: [persistAtom],
+  effects_UNSTABLE: [persistAtom],
 });
-
-// user: {
-//   id: '03478592-accd-4b67-b756-04859fcc23d9',
-//   aud: 'authenticated',
-//   role: 'authenticated',
-//   email: 'yuuta09090530@icloud.com',
-//   phone: '',
-//   confirmation_sent_at: '2023-01-25T06:57:48.5433671Z',
-//   app_metadata: { provider: 'email', providers: [Array] },
-//   user_metadata: {},
-//   identities: [ [Object] ],
-//   created_at: '2023-01-25T06:57:48.538342Z',
-//   updated_at: '2023-01-25T06:57:50.994384Z'
-// },
