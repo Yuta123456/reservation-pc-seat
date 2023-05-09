61b89d8eae1e8c98661ce458e52a017394fdc5ba
Yuta123456 <yuuta09090530@icloud.com>
2023-05-09 21:06:45 +0900

    一応できたはず

--- prisma/migrations/20230430070043_init/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "Log" (
-    "id" SERIAL NOT NULL,
-    "message" TEXT NOT NULL,
-    "level" TEXT NOT NULL,
-    "timestamp" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-
-    CONSTRAINT "Log_pkey" PRIMARY KEY ("id")
-);
